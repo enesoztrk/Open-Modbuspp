c10bfdd3cb567a2390fae10c0c8df213bd090918
Enes <enes.ozturk645@gmail.com>
2020-12-26 17:55:37 +0300

    sonarcloud and travis scripts has been added

---
+++ scripts/cmake_and_sonarqube.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo 'Cmake compilation and Sonarcloud scripts started...'
+# Exit with nonzero exit code if anything fails
+set -e
+
+
+
+
+$CMAKE_PATH  --build $CMAKE_CACHE_PATH --target clean
+
+build-wrapper-linux-x86-64 --out-dir $(pwd)/bw_output  $CMAKE_PATH  --build $CMAKE_CACHE_PATH --target Modbuspp
+
+sonar-scanner -Dproject.settings=$TRAVIS_PROJECT_DIRls/sonar-project.properties
