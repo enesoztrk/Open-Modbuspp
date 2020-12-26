848b6c124383bed60f2da3403a75faeaa54796a4
Enes <enes.ozturk645@gmail.com>
2020-12-27 01:51:25 +0300

    gtest freamwork is added

--- Modbuspp/UnitTests/main.cpp
+++ Modbuspp/UnitTests/main.cpp
@@ -1,9 +1,8 @@
-#include <iostream>
+#include "gtest/gtest.h"
 
-using namespace std;
-
-int main()
+int main(int argc, char **argv)
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    ::testing::InitGoogleTest(&argc, argv);
+    int ret = RUN_ALL_TESTS();
+    return ret;
 }
