6bd1ce10570f0627f2326afcf5046d90918683aa
Enes <enes.ozturk645@gmail.com>
2020-12-26 19:25:48 +0300

    updates

--- scripts/cmake_and_sonarqube.sh
+++ scripts/cmake_and_sonarqube.sh
@@ -10,10 +10,10 @@ mkdir build
 cd build
 cmake -S ../Modbuspp/ -B . -G "Unix Makefiles" -DCMAKE_BUILD_TYPE:String=Debug \
 -DCMAKE_C_COMPILER:STRING=/usr/bin/gcc -DCMAKE_CXX_COMPILER:STRING=/usr/bin/g++
+cd ..
 
+cmake --build build/ --target clean
 
-cmake --build $CMAKE_CACHE_PATH --target clean
-
-build-wrapper-linux-x86-64 --out-dir bw_output  $CMAKE_PATH  --build build/ --target Modbuspp
+build-wrapper-linux-x86-64 --out-dir bw_output  cmake  --build build/ --target Modbuspp
 
 sonar-scanner -Dproject.settings=$TRAVIS_BUILD_DIR/sonar-project.properties
