b8db8fb8b02d893bf99328b597a278d677656ab9
Enes <enes.ozturk645@gmail.com>
2020-12-27 13:02:26 +0300

    port folder added and .gitignore file updated to ignore build outputs

--- scripts/cmake_and_sonarqube.sh
+++ scripts/cmake_and_sonarqube.sh
@@ -8,8 +8,8 @@ set -e
 cd $TRAVIS_BUILD_DIR/Modbuspp
 mkdir build
 cd build
-cmake -S ../Modbuspp/ -B . -G "Unix Makefiles" -DCMAKE_BUILD_TYPE:String=Debug \
--DCMAKE_C_COMPILER:STRING=/usr/bin/gcc -DCMAKE_CXX_COMPILER:STRING=/usr/bin/g++ ..
+cmake -S ../Modbuspp/ -B .  -DCMAKE_BUILD_TYPE:String=Debug \
+-DCMAKE_PLATFORM:String=POSIX -DPROJECT_TYPE:String=UNIT-TEST -DCMAKE_C_COMPILER:STRING=/usr/bin/gcc -DCMAKE_CXX_COMPILER:STRING=/usr/bin/g++ ..
 cd ..
 
 
@@ -17,6 +17,6 @@ NUMBER_OF_PROCESSORS=$(nproc --all)
 
 cmake --build build/ --target clean
 
-build-wrapper-linux-x86-64 --out-dir bw_output  cmake  --build build/ --target Modbuspp
+build-wrapper-linux-x86-64 --out-dir bw_output  cmake  --build build/ --target all
 
 sonar-scanner -Dproject.settings=$TRAVIS_BUILD_DIR/sonar-project.properties  -Dsonar.cfamily.threads=${NUMBER_OF_PROCESSORS}
